0a7a52dc2bec07f0eaa13c7e287439290c8e86ea
Barrett Allen <bear9151@icloud.com>
2022-09-05 18:42:49 -0500

    Updating game.js in testing for lock button

--- js/game.js
+++ js/game.js
@@ -169,42 +169,42 @@ $("#lock-button").click(function() {
 		}
 	});
 	if (wordCheck.length > 0) {
-		const newAlphabet = {
-  a: 1,
-  e: 1,
-  i: 1,
-  o: 1,
-  u: 1,
-  l: 1,
-  n: 1,
-  r: 1,
-  s: 1,
-  t: 1,
-  d: 2,
-  g: 2,
-  b: 3,
-  c: 3,
-  m: 3,
-  p: 3,
-  f: 4,
-  h: 4,
-  v: 4,
-  w: 4,
-  y: 4,
-  k: 5,
-  j: 8,
-  x: 8,
-  q: 10,
-  z: 10,
-};
+// 		const newAlphabet = {
+//   a: 1,
+//   e: 1,
+//   i: 1,
+//   o: 1,
+//   u: 1,
+//   l: 1,
+//   n: 1,
+//   r: 1,
+//   s: 1,
+//   t: 1,
+//   d: 2,
+//   g: 2,
+//   b: 3,
+//   c: 3,
+//   m: 3,
+//   p: 3,
+//   f: 4,
+//   h: 4,
+//   v: 4,
+//   w: 4,
+//   y: 4,
+//   k: 5,
+//   j: 8,
+//   x: 8,
+//   q: 10,
+//   z: 10,
+// };
 
-const scrabbleScore = word =>
-  word
-    .split('')
-    .map(letter => newAlphabet[letter])
-    .reduce((a, b) => a + b);
+// const scrabbleScore = word =>
+//   word
+//     .split('')
+//     .map(letter => newAlphabet[letter])
+//     .reduce((a, b) => a + b);
 
-		// prompt("your word works with a score of " + score + "!");
+		prompt("your word works with a score of " + score + "!");
 	} else {
 		prompt("that word doesn't work :(");
 	}
